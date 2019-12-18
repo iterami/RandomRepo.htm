5fb122c3f3fe5d2e6ab6adb201971882046b48f4
Jan Hořava <czechinthemail@gmail.com>
2019-12-17 19:52:11 -0800

    added iterami/clearundostack

--- js/data.js
+++ js/data.js
@@ -56,6 +56,7 @@ function random_github(){
       'Chromium-iterami-DevTools': 'https://github.com/iterami/Chromium-iterami-DevTools',
       'Chromium-iterami-Extension': 'https://github.com/iterami/Chromium-iterami-Extension',
       'Chromium-iterami-Theme': 'https://github.com/iterami/Chromium-iterami-Theme',
+      'clearundostack': 'https://github.com/iterami/clearundostack',
       'ConfigFiles': 'https://github.com/iterami/ConfigFiles',
       'EVE-Overviews.yaml': 'https://github.com/iterami/EVE-Overviews.yaml',
       'EVETools.gtk': 'https://github.com/iterami/EVETools.gtk',
